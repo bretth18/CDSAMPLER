0f8e23c330cae495b837f37d1ee8e8768d5ed5dc
Brett Henderson <bretth18@gmail.com>
2021-01-16 16:43:14 -0800

    Initial Commit

---
+++ CDSAMPLER/CDSAMPLERApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDSAMPLERApp.swift
+//  CDSAMPLER
+//
+//  Created by Brett Henderson on 1/16/21.
+//
+
+import SwiftUI
+
+@main
+struct CDSAMPLERApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CDSAMPLER/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CDSAMPLER
+//
+//  Created by Brett Henderson on 1/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
